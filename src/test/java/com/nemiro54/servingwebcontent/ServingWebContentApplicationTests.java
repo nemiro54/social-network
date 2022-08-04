e825841cae083adf01bfca6eb349cad4e16d499a
Uladzislau Niamira <nemiro54@gmail.com>
2022-08-04 19:29:38 +0300

    feature(SN-0) added the skeleton of a Spring Boot project

---
+++ src/test/java/com/nemiro54/servingwebcontent/ServingWebContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemiro54.servingwebcontent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServingWebContentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
