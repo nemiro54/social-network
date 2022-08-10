a39da29263d3082aaf11a646228e4b5678dc1dec
Uladzislau Niamira <89138081+nemiro54@users.noreply.github.com>
2022-08-10 13:14:26 +0300

    SN-1: A database added to the project (#4)
    * feature(SN-1) added main page
    
    * feature(SN-1) Added database to the project
    
    * docs(SN-1) updated RELEASE_NOTES.md
    
    * bugs(SN-1) deleted ServingWebContentApplicationTests.java

--- src/main/java/com/nemiro54/socialnetwork/GreetingController.java
+++ src/main/java/com/nemiro54/socialnetwork/GreetingController.java
@@ -1,24 +1,65 @@
 package com.nemiro54.socialnetwork;
 
+import com.nemiro54.socialnetwork.domain.Message;
+import com.nemiro54.socialnetwork.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    private final MessageRepository messageRepository;
+
+    @Autowired
+    public GreetingController(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World")
-                               String name, Map<String, Object> model) {
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
+                           Map<String, Object> model) {
         model.put("name", name);
         return "greeting";
     }
 
     @GetMapping
     public String main(Map<String, Object> model) {
-        model.put("some", "Hi there, my name Uladzislau. I'm a Java Developer. This is my first web application");
+        Iterable<Message> messages = messageRepository.findAll();
+
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String  text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+
+        messageRepository.save(message);
+
+        Iterable<Message> messages = messageRepository.findAll();
+
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+
+        model.put("messages", messages);
+
         return "main";
     }
 }
