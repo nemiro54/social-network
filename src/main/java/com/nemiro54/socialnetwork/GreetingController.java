3206c8032e515e05c707041bb9a1baf5b07dd2fc
Uladzislau Niamira <nemiro54@gmail.com>
2022-08-10 12:59:54 +0300

    feature(SN-1) Added database to the project

--- src/main/java/com/nemiro54/socialnetwork/GreetingController.java
+++ src/main/java/com/nemiro54/socialnetwork/GreetingController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Iterator;
 import java.util.Map;
 
 @Controller
@@ -21,8 +20,8 @@ public class GreetingController {
     }
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World")
-                               String name, Map<String, Object> model) {
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
+                           Map<String, Object> model) {
         model.put("name", name);
         return "greeting";
     }
@@ -40,7 +39,24 @@ public class GreetingController {
     public String add(@RequestParam String  text, @RequestParam String tag, Map<String, Object> model) {
         Message message = new Message(text, tag);
 
+        messageRepository.save(message);
+
         Iterable<Message> messages = messageRepository.findAll();
+
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
 
         model.put("messages", messages);
 
--- src/main/java/com/nemiro54/socialnetwork/repository/MessageRepository.java
+++ src/main/java/com/nemiro54/socialnetwork/repository/MessageRepository.java
@@ -3,5 +3,10 @@ package com.nemiro54.socialnetwork.repository;
 import com.nemiro54.socialnetwork.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepository extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
+
 }
