e8562418da671061091dbc221dcfd8b929bcd835
Uladzislau Niamira <nemiro54@gmail.com>
2022-09-06 00:26:23 +0300

    feature(SN-5) added the ability to upload files to the server

--- src/main/java/com/nemiro54/socialnetwork/domain/Message.java
+++ src/main/java/com/nemiro54/socialnetwork/domain/Message.java
@@ -3,27 +3,29 @@ package com.nemiro54.socialnetwork.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
 public class Message {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-
     private String text;
     private String tag;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    private String fileName;
 
     public Message() {
     }
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, User author) {
         this.text = text;
         this.tag = tag;
+        this.author = author;
     }
 }
